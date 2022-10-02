fb9e556a6466d46b97b0a232d271cdd608f4b455
Oyalmli <oyvind.almli@gmail.com>
2022-10-02 11:17:01 +0200

    Updated dfl version

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,13 @@
 #include "dfl/dfl.hpp"
 
 using namespace dfl;
-int main() {
-    auto generator = gen::range(10);
-    auto modified_generator = 
-        mod::take  (100, 
-        mod::repeat(generator));
-    
+int main() {    
     auto pipeline
     =   pipe::stride(2)
     >>= pipe::transform([](auto i){ return i*i; });
 
-    auto sink = sink::print("\n");
-
-    modified_generator >>= pipeline >>= sink;
+    mod::take  (100, 
+    mod::repeat(gen::range(10)))
+    >>= pipeline 
+    >>= sink::print<'\n'>();
 }
